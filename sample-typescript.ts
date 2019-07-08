ba7f121d170454e51fbbaf7ce81a19aab4f66b0e
Daniel Zeiter <daniel.zeiter@ergon.ch>
2019-07-08 12:43:17 +0200

    Add tslint

--- sample-typescript.ts
+++ sample-typescript.ts
@@ -1,18 +1,18 @@
-interface User {
-    firstName: string,
-    lastName: string,
-    birthyear: number
+interface IUser {
+    firstName: string;
+    lastName: string;
+    birthyear: number;
 }
 
-const user: User = {
-    firstName: "Hans",
-    lastName: "Meier",
-    birthyear: 1970
-};
-
-function hello_from_typescript(user: User, currentYear = 2019) {
+function hello_from_typescript(user: IUser, currentYear = 2019) {
     const age = currentYear - user.birthyear;
     console.log(`Hello ${user.firstName} ${user.lastName}. You will turn ${age} this year. From Typescript.`);
 }
 
-hello_from_typescript(user, "2019");
+const demoUser: IUser = {
+    birthyear: 1970,
+    firstName: "Hans",
+    lastName: "Meier",
+};
+
+hello_from_typescript(demoUser);
--- sample.js
+++ sample.js
@@ -1,12 +1,12 @@
+function hello(user, currentYear = 2019) {
+    const age = currentYear - user.birthyear;
+    console.log(`Hello ${user.firstName} ${user.lastName}. You will turn ${age} this year`);
+}
+
 const user = {
     firstName: "Hans",
     lastName: "Meier",
     birthyear: 1970
 };
-
-function hello(user, currentYear = 2019) {
-    const age = currentYear - user.birthyear;
-    console.log(`Hello ${user.firstName} ${user.lastName}. You will turn ${age} this year`);
-}
 
 hello(user);
